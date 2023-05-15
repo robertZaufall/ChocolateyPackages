466ff2875a8d2c435f3b6eb18dfd542fdc16c43d
robertZaufall <mail@zaufall.de>
2023-05-15 16:46:09 +0200

    Initial commit

---
+++ xsddiagram/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName  = 'xsddiagram'
+$toolsPath    = Split-Path $MyInvocation.MyCommand.Definition
+$fileFullPath = Join-Path $toolsPath 'XSDDiagram.zip'
+$destination  = Join-Path ${env:ProgramFiles(x86)} 'XSDDiagram'
+
+$packageArgs = @{
+  packageName            = $packageName
+  filefullpath           = $fileFullPath
+  destination            = $destination
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$destinationLink = Join-Path 'C:\Users\Public\Desktop' 'XSDDiagram.lnk'
+$destinationExe  = Join-Path $destination 'XSDDiagram.exe'
+Install-ChocolateyShortcut -ShortcutFilePath $destinationLink -TargetPath $destinationExe
---
+++ xsemmel/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName  = 'xsemmel'
+$toolsPath    = Split-Path $MyInvocation.MyCommand.Definition
+$fileFullPath = "$toolsPath\xsemmel-portable_2017-04-09.zip"
+$destination  = Join-Path ${env:ProgramFiles(x86)} 'Xsemmel'
+
+$packageArgs = @{
+  packageName            = $packageName
+  filefullpath           = $fileFullPath
+  destination            = $destination
+}
+
+Get-ChocolateyUnzip @packageArgs
+
+$destinationLink = Join-Path 'C:\Users\Public\Desktop' 'Xsemmel.lnk'
+$destinationExe  = Join-Path $destination 'Xsemmel.exe'
+Install-ChocolateyShortcut -ShortcutFilePath $destinationLink -TargetPath $destinationExe
