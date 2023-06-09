bf8c5e4bd03de54340ecb1ff2abd2c65f1f69d72
robertZaufall <mail@zaufall.de>
2023-06-09 03:50:12 +0200

    fix verification, sha and licence

--- chocolatestore/tools/chocolateyInstall.ps1
+++ chocolatestore/tools/chocolateyInstall.ps1
@@ -5,9 +5,11 @@ $destination  = Join-Path ${env:ProgramFiles(x86)} 'ChocolateStore'
 $exeName      = 'ChocolateStoreCore.exe'
 
 $packageArgs = @{
-  packageName            = $packageName
-  filefullpath           = $fileFullPath
-  destination            = $destination
+  packageName    = $packageName
+  filefullpath   = $fileFullPath
+  destination    = $destination
+  checksum       = 'cb07a6568bc6acaf21b1184cffc5bd6b07d8ec0c9ef25127299f456daa0879b9'
+  checksumType   = 'sha256'                
 }
 
 Get-ChocolateyUnzip @packageArgs
