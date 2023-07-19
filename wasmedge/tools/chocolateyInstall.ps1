876c6255675acf04f474a4b610a4351ed2ee82a3
robertZaufall <mail@zaufall.de>
2023-07-19 19:29:00 +0200

    updated to version 0.13.1

--- wasmedge/tools/chocolateyInstall.ps1
+++ wasmedge/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
 	packageName    = 'wasmedge'
-	url            = 'https://github.com/WasmEdge/WasmEdge/releases/download/0.13.0/WasmEdge-0.13.0-windows.msi'
+	url            = 'https://github.com/WasmEdge/WasmEdge/releases/download/0.13.1/WasmEdge-0.13.1-windows.msi'
 	fileType       = 'msi'
 	silentArgs     = '/quiet /norestart'
-    checksum       = '25637dc88f088b840bb23d37e1fb87eaf27ac35a0866da9a4339b3957d3912e9'
+    checksum       = '671aab6918a0acf4e3e91ce5d8a9c816b7f4c55dbdf9727cb9f362e8cbb96624'
     checksumType   = 'sha256'
 	validExitCodes = @(0, 3010, 1641)
 }
