bf4ce8371a584e3728ccb21d66e86199e4a0f790
robertZaufall <mail@zaufall.de>
2023-08-11 23:36:34 +0200

    update bravo to 1.0.5

--- bravo/tools/chocolateyInstall.ps1
+++ bravo/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
 	packageName    = 'bravo'
-	url            = 'https://github.com/sql-bi/Bravo/releases/download/v1.0.4/Bravo.1.0.4.x64.msi'
+	url            = 'https://github.com/sql-bi/Bravo/releases/download/v1.0.5/Bravo.1.0.5.x64.msi'
 	fileType       = 'msi'
 	silentArgs     = '/quiet /norestart'
-    checksum       = '78bfe6fdac292767507f3f146f1077040927abe99b84a0324b186dc0623061a2'
+    checksum       = '8845a4d150f6e038b916c5aec36c842c00613a0359d9b7f3af488b6412eff6dc'
     checksumType   = 'sha256'
 	validExitCodes = @(0, 3010, 1641)
 }
