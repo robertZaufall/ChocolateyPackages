816a851a30428934b6ff741677369fe7e66088db
robertZaufall <mail@zaufall.de>
2023-09-22 18:45:06 +0200

    Update PowerBI Gateway

--- powerbigateway/tools/chocolateyInstall.ps1
+++ powerbigateway/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
 	url            = 'https://download.microsoft.com/download/D/A/1/DA1FDDB8-6DA8-4F50-B4D0-18019591E182/GatewayInstall.exe'
 	fileType       = 'exe'
 	silentArgs     = '/q'
-    checksum       = '2b6a1183ebf2e7cf37bab101cf205ad4aaf215c2bdcc231bca4effcf2a614d14'
+    checksum       = '3a0b13509b10c567b041c8152f6d0222ee8f003bda2d908fafd5e3224528c20b'
     checksumType   = 'sha256'
 	validExitCodes = @(0, 3010, 1641)
 }
