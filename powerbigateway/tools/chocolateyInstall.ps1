c2284bdc068e8eff5fd3374119b76865c770d305
robertZaufall <mail@zaufall.de>
2023-10-20 02:40:08 +0200

    Update PowerBiGateway package

--- powerbigateway/tools/chocolateyInstall.ps1
+++ powerbigateway/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
 	url            = 'https://download.microsoft.com/download/D/A/1/DA1FDDB8-6DA8-4F50-B4D0-18019591E182/GatewayInstall.exe'
 	fileType       = 'exe'
 	silentArgs     = '/q'
-    checksum       = '3a0b13509b10c567b041c8152f6d0222ee8f003bda2d908fafd5e3224528c20b'
+    checksum       = '6c404aeb4586821df13ad29347a0d91efd8e06b83efface417a45785cec708e9'
     checksumType   = 'sha256'
 	validExitCodes = @(0, 3010, 1641)
 }
