c02dbc11034365d8e1424979bf9df87133fcf99e
robertZaufall <mail@zaufall.de>
2024-01-03 18:58:49 +0100

    update  powerbigateway version

--- powerbigateway/tools/chocolateyInstall.ps1
+++ powerbigateway/tools/chocolateyInstall.ps1
@@ -5,7 +5,7 @@ $packageArgs = @{
 	url            = 'https://download.microsoft.com/download/D/A/1/DA1FDDB8-6DA8-4F50-B4D0-18019591E182/GatewayInstall.exe'
 	fileType       = 'exe'
 	silentArgs     = '/install /quiet /norestart /log OnPremDataGateway-Install.log'
-    checksum       = '3a498c61605156c61071f179cc0eb94afbba8371f0175522e5c88d2045cb01e0'
+    checksum       = 'acfc79ba48a4d7cbf990fa36072d80982eb05af2e0637386a308d043fd956d32'
     checksumType   = 'sha256'
 	validExitCodes = @(0, 3010, 1641)
 }
