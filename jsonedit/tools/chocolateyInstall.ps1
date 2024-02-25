b15a5369b83e15cfcb45c604fb9ae4c6820eb168
robertZaufall <mail@zaufall.de>
2024-02-25 22:07:25 +0100

    update jsonedit to 0.9.42

--- jsonedit/tools/chocolateyInstall.ps1
+++ jsonedit/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName    = 'jsonedit'
-$url            = 'https://tomeko.net/software/JSONedit/bin/JSONedit_0_9_41.zip'
+$url            = 'https://tomeko.net/software/JSONedit/bin/JSONedit_0_9_42.zip'
 $validExitCodes = @(0)
 $exeName        = "jsonedit.exe"
-$checksum       = '16b6dfcf846900182d2519e9186d0e121a842599372127af15be197e2e289b0d'
+$checksum       = '8c46682def02bab0e8946e48bda9dc26476ef6382da2ea37f32010f03fd8cc1f'
 $checksumType   = 'sha256'
 
 Install-ChocolateyZipPackage "$packageName" "$url" "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" -checksum $checksum -checksumType $checksumType
